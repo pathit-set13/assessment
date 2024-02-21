8d6ddd7fb1b051f91f2906facb8d48a5ac87ec11
Pathit <pathitta.se@gmail.com>
2024-02-21 18:32:02 +0700

    Update user_account.sql
    add role value

--- posttest/src/main/resources/db/user_account.sql
+++ posttest/src/main/resources/db/user_account.sql
@@ -1,2 +1,2 @@
-INSERT INTO user_account (username, password) VALUES ('admin', 'password');
-INSERT INTO user_account (username, password) VALUES ('member', 'password');
+INSERT INTO user_account (username, password) VALUES ("admin", "password", "ADMIN");
+INSERT INTO user_account (username, password) VALUES ("member", "password", "MEMBER");
