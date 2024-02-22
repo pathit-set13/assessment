6057951fdb41580c7291184f83fb3163d1dc9544
Pathit <pathitta.se@gmail.com>
2024-02-22 19:17:18 +0700

    Create LotteryController

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/LotteryController.java
@@ -0,0 +1,24 @@
+package com.kbtg.bootcamp.posttest.lottery;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lotteries")
+public class LotteryController {
+
+    private final LotteryService lotteryService;
+
+    public LotteryController(LotteryService lotteryService) {
+        this.lotteryService = lotteryService;
+    }
+
+    @GetMapping("")
+    public List<Lottery> getLotteryList() {
+        return this.lotteryService.getLotteryList();
+    }
+
+}
