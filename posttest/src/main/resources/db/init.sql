d666109621c3a588ec97b8fd010e124d13f92128
Pathit <pathitta.se@gmail.com>
2024-02-24 17:26:12 +0700

    Create init file

---
+++ posttest/src/main/resources/db/init.sql
@@ -0,0 +1,38 @@
+--Drop tables if they exist
+--DROP TABLE IF EXISTS lottery CASCADE;
+--DROP TABLE IF EXISTS user_ticket CASCADE;
+--DROP TABLE IF EXISTS user_account CASCADE;
+
+--Create tables
+CREATE TABLE lottery (
+    ticketId SERIAL PRIMARY KEY,
+    ticket VARCHAR(6) UNIQUE NOT NULL,
+    price INTEGER NOT NULL,
+    amount INTEGER NOT NULL
+);
+
+CREATE TABLE user_account (
+    userId SERIAL PRIMARY KEY,
+    user_name VARCHAR(30) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL,
+    user_role VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_ticket (
+    userId INTEGER,
+    ticket VARCHAR(6),
+    amount INTEGER,
+    FOREIGN KEY (userId) REFERENCES user_account(userId),
+    FOREIGN KEY (ticket) REFERENCES lottery(ticket)
+);
+
+--Initial data
+INSERT INTO lottery(ticket, price, amount)
+VALUES
+    ('000000', 80, 1),
+    ('999999', 80, 1);
+
+INSERT INTO user_account (user_name, user_password, user_role)
+VALUES
+    ('admin', 'password', 'ADMIN'),
+    ('member', 'password', 'MEMBER');
