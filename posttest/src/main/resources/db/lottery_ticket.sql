ef1ae65d686766bd1d0c479a062fc534e8aa0572
Pathit <pathitta.se@gmail.com>
2024-02-26 21:30:29 +0700

    delete data mock up

--- posttest/src/main/resources/db/lottery_ticket.sql
+++
@@ -1,2 +0,0 @@
---INSERT INTO lottery(ticket, price, amount) VALUES ("000000", 80, 1);
---INSERT INTO lottery(ticket, price, amount) VALUES ("999999", 80, 1);
--- posttest/src/main/resources/db/user_account.sql
+++
@@ -1,2 +0,0 @@
---INSERT INTO user_account (username, password) VALUES ("admin", "password", "ADMIN");
---INSERT INTO user_account (username, password) VALUES ("member", "password", "MEMBER");
